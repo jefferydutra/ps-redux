eefcd4ecc5be0d8e242c6d497eee5070da776e30
Cory House <housecor@gmail.com>
2016-03-07 11:05:25 -0800

    Simplified header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
-const Header = () => {
+export default function Header() {
 	return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-            <IndexLink to="/" className="navbar-brand" activeClassName="active">
-              <img src="images/pluralsight-logo.png" />
-            </IndexLink>
-            <ul className="nav navbar-nav">
-              <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
-              <li><Link to="/authors" activeClassName="active">Authors</Link></li>
-              <li><Link to="/about" activeClassName="active">About</Link></li>
-            </ul>
-        </div>
+      <nav>
+          <IndexLink to="/" activeClassName="active">Home</IndexLink> {" | "}
+          <Link to="/authors" activeClassName="active">Authors</Link> {" | "}
+          <Link to="/about" activeClassName="active">About</Link>
       </nav>
 	);
-};
-
-module.exports = Header;
+}
