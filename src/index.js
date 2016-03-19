1cb1d25d3b7275cb4c3ff10e29e56e5b48da6b2b
Cory House <housecor@gmail.com>
2016-03-19 10:42:20 -0500

    Eliminated SASS dependency.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './store/configureStore';
 import routes from './routes';
 //import '../node_modules/bootstrap/less/bootstrap.less';
-import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import './styles/styles.css'; //Yep, that's right. You can import CSS files too! Webpack will run the associated loader and plug this into the page.
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
