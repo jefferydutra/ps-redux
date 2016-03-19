3a39589f298b9e42f49d2a9d24b6e8379507b67f
Cory House <housecor@gmail.com>
2016-03-19 15:24:43 -0500

    Tweaked comments

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -1,16 +1,12 @@
 // This script copies src/index.html into /dist/index.html
 // This is a good example of using Node and cheerio to do a simple file transformation.
 // In this case, the transformation is useful since we only use a separate css file in prod.
-
-// Allowing console calls below since this is a build file.
-/*eslint-disable no-console */
-
 import fs from 'fs';
 import cheerio from 'cheerio';
 
 fs.readFile('src/index.html', 'utf8', (err, markup) => {
   if (err) {
-    return console.log(err);
+    return console.log(err); //eslint-disable-line no-console
   }
 
   const $ = cheerio.load(markup);
@@ -20,7 +16,7 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
 
   fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
     if (err) {
-      return console.log(err);
+      return console.log(err); //eslint-disable-line no-console
     }
   });
 
