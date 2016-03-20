e12a40aaab3e92d88887e0f97f269da28b08413d
Cory House <housecor@gmail.com>
2016-03-20 08:17:10 -0500

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import 'babel-polyfill';
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './store/configureStore';
 import routes from './routes';
-//import '../node_modules/bootstrap/less/bootstrap.less';
-import './styles/styles.css'; //Yep, that's right. You can import CSS files too! Webpack will run the associated loader and plug this into the page.
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; //Webpack can import CSS files too!
+import './styles/styles.css';
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
