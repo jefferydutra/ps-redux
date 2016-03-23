a88a4d1ef57ea5839f297788c9a3f03e0e77382d
Cory House <housecor@gmail.com>
2016-03-23 05:44:58 -0500

    Fixed nesting structure

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,11 +4,9 @@ import { Link } from 'react-router';
 const HomePage = () => {
   return (
     <div>
-      <div className="container-fluid">
-        <h1>Pluralsight Administration</h1>
-        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
-      </div>
+      <h1>Pluralsight Administration</h1>
+      <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+      <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
     </div>
   );
 };
