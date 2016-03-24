8b2af8e0b8e2171e67e80d8187790bf2c647f697
Cory House <housecor@gmail.com>
2016-03-23 21:36:57 -0500

    Enhanced layout

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
       <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
       <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
