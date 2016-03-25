cfc1c9612b71ca947cd78926da55e91d23d20cfb
Cory House <housecor@gmail.com>
2016-03-24 21:19:30 -0500

    Changed App component to class

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import Header from './common/Header';
 
-const App = ({children, loading}) => {
-  return (
-    <div className="container-fluid">
-      <Header loading={loading} />
-      {children}
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Header loading={this.props.loading}/>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
   children: PropTypes.object.isRequired,
