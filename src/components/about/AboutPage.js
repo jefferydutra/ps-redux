ede7593aded466427301f7067f911182ec2c2efd
Cory House <housecor@gmail.com>
2016-03-24 21:01:24 -0500

    Converted top-level components to classes so they'll support hot reloading.

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,16 +1,17 @@
-// Since the about page needs no
-// data or actions, this doesn't
+// Since the about page needs no data or actions, this doesn't
 // contain the usual Redux container boilerplate
 // for mapState and mapDispatch.
 import React from 'react';
 
-const AboutPage = () => {
+class AboutPage extends React.Component {
+  render() {
     return (
       <div>
         <h1>About</h1>
         <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
       </div>
     );
-};
+  }
+}
 
 export default AboutPage;
