a8c7bf7f7f3eed0df58443232553ab1c8d85ae62
Cory House <housecor@gmail.com>
2016-03-25 06:23:07 -0500

    Added sorting to courses

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -4,6 +4,10 @@ const initialState = {
     courses: []
 };
 
+export function sortByTitle(courses) {
+  return courses.sort((a, b) => a.title > b.title);
+}
+
 // IMPORTANT: Note that with Redux, state should NEVER be changed.
 // State is considered immutable. Instead,
 // create a copy of the state passed and set new values on the copy.
@@ -13,12 +17,12 @@ export default function courseReducer(state = initialState, action) {
   switch (action.type) {
     case types.LOADED_COURSES:
       return Object.assign({}, state, {
-        courses: action.courses
+        courses: sortByTitle(action.courses)
       });
 
     case types.CREATED_COURSE:
      return Object.assign({}, state, {
-       courses: [...state.courses, action.course]
+       courses: sortByTitle([...state.courses, action.course])
      });
 
     case types.UPDATED_COURSE:
@@ -27,10 +31,10 @@ export default function courseReducer(state = initialState, action) {
       // newState = Object.assign({}, state, { loading: false });
       // newState.courses.splice(existingAuthorIndex, 1, action.course);
       return Object.assign({}, state, {
-        courses: [
+        courses: sortByTitle([
           ...state.courses.filter(course => course.id !== action.course.id),
           action.course
-        ]
+        ])
       });
 
     case types.DELETED_COURSE:
