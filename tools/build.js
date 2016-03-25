638ee2eef67bbd401a8ca94fb41a5fbeb495e575
Cory House <housecor@gmail.com>
2016-03-25 07:10:16 -0500

    Simplified production JS build

--- tools/build.js
+++ tools/build.js
@@ -4,22 +4,16 @@
 import webpack from 'webpack';
 import webpackConfigBuilder from '../webpack.config';
 import colors from 'colors';
-import { argv as args } from 'yargs';
 
 process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
 
 const webpackConfig = webpackConfigBuilder(process.env.NODE_ENV);
 
+console.log('Generating minified bundle for production use via Webpack. This will take a moment...'.bold.blue);
+
 webpack(webpackConfig).run((err, stats) => {
-  const inSilentMode = args.s; // set to true when -s is passed on the command
-
-  if (!inSilentMode) {
-    console.log('Generating minified bundle for production use via Webpack...'.bold.blue);
-  }
-
   if (err) { // so a fatal error occurred. Stop here.
     console.log(err.bold.red);
-
     return 1;
   }
 
@@ -29,14 +23,12 @@ webpack(webpackConfig).run((err, stats) => {
     return jsonStats.errors.map(error => console.log(error.red));
   }
 
-  if (jsonStats.hasWarnings && !inSilentMode) {
+  if (jsonStats.hasWarnings) {
     console.log('Webpack generated the following warnings: '.bold.yellow);
     jsonStats.warnings.map(warning => console.log(warning.yellow));
   }
 
-  if (!inSilentMode) {
-    console.log(`Webpack stats: ${stats}`);
-  }
+  console.log(`Webpack stats: ${stats}`);
 
   // if we got this far, the build succeeded.
   console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'.green.bold);
