8af02059581bf1233890bfdad5ec1090864eea44
Cory House <housecor@gmail.com>
2016-03-30 11:43:54 -0500

    Formatting cleanup

--- src/components/author/AuthorList.js
+++ src/components/author/AuthorList.js
@@ -1,24 +1,26 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+import { Link } from 'react-router';
 
 const AuthorList = ({authors, deleteAuthor}) => {
   return (
     <table className="table">
       <thead>
         <tr>
-          <th></th>
+          <th>&nbsp;</th>
           <th>ID</th>
           <th>Name</th>
         </tr>
       </thead>
       <tbody>
-        {authors.map((author) => {
-          return (
-            <tr key={author.id}>
-            <td><a href="#" onClick={(event) => deleteAuthor(event, author.id)}>Delete</a></td>
-            <td><Link to={'/author/' + author.id}>{author.id}</Link></td>
-            <td>{author.firstName} {author.lastName}</td>
-          </tr>);
+        {
+          authors.map((author) => {
+            return (
+              <tr key={author.id}>
+                <td><a href="#" onClick={(event) => deleteAuthor(event, author.id)}>Delete</a></td>
+                <td><Link to={'/author/' + author.id}>{author.id}</Link></td>
+                <td>{author.firstName} {author.lastName}</td>
+              </tr>
+            );
           })
         }
       </tbody>
