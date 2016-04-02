f18bbac8daf0d0bd01136aea26b290bc4860320f
Cory House <housecor@gmail.com>
2016-04-02 11:35:48 -0500

    Renamed

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
-import AuthorPage from './components/author/AuthorPage';
+import AuthorsPage from './components/author/AuthorsPage';
 import ManageAuthorPage from './components/author/ManageAuthorPage';
-import CoursePage from './components/course/CoursePage';
+import CoursesPage from './components/course/CoursesPage';
 import ManageCoursePage from './components/course/ManageCoursePage';
 import AboutPage from './components/about/AboutPage';
 import NotFoundPage from './components/NotFoundPage';
@@ -12,10 +12,10 @@ import NotFoundPage from './components/NotFoundPage';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    <Route path="authors" component={AuthorPage} />
+    <Route path="authors" component={AuthorsPage} />
     <Route path="author/:id" component={ManageAuthorPage} />
     <Route path="author" component={ManageAuthorPage} />
-    <Route path="courses" component={CoursePage} />
+    <Route path="courses" component={CoursesPage} />
     <Route path="course/:id" component={ManageCoursePage} />
     <Route path="course" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
