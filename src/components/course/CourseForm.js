f1c1f4f96ea4ce9b18fa8da2c9967c1d9ae5db76
Cory House <housecor@gmail.com>
2016-04-03 07:31:49 -0500

    Formatting tweak

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -6,27 +6,30 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => 
 	return (
 		<form>
 			<h1>Manage Course</h1>
-			<TextInput name="title"
-				label="Title"
-				value={course.title}
-				onChange={onChange}
-				error={errors.title} />
+			<TextInput
+        name="title"
+        label="Title"
+        value={course.title}
+        onChange={onChange}
+        error={errors.title} />
 
-			<SelectInput name="authorId"
-				label="Author"
-				value={course.authorId}
-				defaultOption="Select Author"
-				options={allAuthors}
-				onChange={onChange}
-                error={errors.authorId} />
+			<SelectInput
+        name="authorId"
+        label="Author"
+        value={course.authorId}
+        defaultOption="Select Author"
+        options={allAuthors}
+        onChange={onChange} error={errors.authorId} />
 
-			<TextInput name="category"
-				label="Category"
-				value={course.category}
-				onChange={onChange}
-				error={errors.category} />
+			<TextInput
+        name="category"
+        label="Category"
+        value={course.category}
+        onChange={onChange}
+        error={errors.category} />
 
-			<TextInput name="length"
+			<TextInput
+        name="length"
 				label="Length"
 				value={course.length}
 				onChange={onChange}
