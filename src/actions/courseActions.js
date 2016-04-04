6eb990d7f658fae38f68be99eaf29f6e4dcc8c51
Cory House <housecor@gmail.com>
2016-04-04 06:07:43 -0500

    Added example of getting state from store within thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -3,43 +3,47 @@ import * as types from '../constants/actionTypes';
 import {loading, loadingComplete} from './loadingActions';
 
 export function loadedCourses(courses) {
-	return { type: types.LOADED_COURSES, courses };
+  return {type: types.LOADED_COURSES, courses};
 }
 
 export function createdCourse(course) {
-	return { type: types.CREATED_COURSE, course };
+  return {type: types.CREATED_COURSE, course};
 }
 
 export function updatedCourse(course) {
-	return { type: types.UPDATED_COURSE, course };
+  return {type: types.UPDATED_COURSE, course};
 }
 
 export function deletedCourse(id) {
-	return { type: types.DELETED_COURSE, id };
+  return {type: types.DELETED_COURSE, id};
 }
 
 // Functions below handle asynchronous calls.
 // Each returns a function that accepts a dispatch.
 // These are used by redux-thunk to support asynchronous interactions.
 export function loadCourses() {
-	return function(dispatch) {
-		dispatch(loading());
-		return CourseApi.getAllCourses().then( courses => {
-			dispatch(loadedCourses(courses));
+  return function (dispatch) {
+    dispatch(loading());
+    return CourseApi.getAllCourses().then(courses => {
+      dispatch(loadedCourses(courses));
       dispatch(loadingComplete());
-    }).catch( error => {
+    }).catch(error => {
       dispatch(loadingComplete());
       throw(error);
-    });	};
+    });
+  };
 }
 
 export function saveCourse(course) {
-	return function(dispatch) {
-		dispatch(loading());
-		return CourseApi.saveCourse(course).then( course => {
+  // Note that I you can get a handle on store state using the 2nd param.
+  // This may mean you don't have to pass data into the thunk
+  return function (dispatch, getState) {
+    // const state = getState(); //example of getting state from store
+    dispatch(loading());
+    return CourseApi.saveCourse(course).then(course => {
       course.id ? dispatch(updatedCourse(course)) : dispatch(createdCourse(course));
       dispatch(loadingComplete());
-    }).catch( error => {
+    }).catch(error => {
       dispatch(loadingComplete());
       throw(error);
     });
@@ -51,11 +55,11 @@ export function saveCourse(course) {
 // Since the UI will be immediately updated
 // The user will only be notified if there's an error.
 export function deleteCourse(courseId) {
-	return function(dispatch) {
+  return function (dispatch) {
     dispatch(loading());
-		return CourseApi.deleteCourse(courseId).then( () => {
+    return CourseApi.deleteCourse(courseId).then(() => {
       dispatch(deletedCourse(courseId));
       dispatch(loadingComplete());
     });
-	};
+  };
 }
